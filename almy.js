2e68db616b042424838906214dc83b9515ff26b2
Tomás <tomas2387@gmail.com>
2018-01-28 22:57:41 +0100

    Standard JavaScript Style Guide

--- almy.js
+++ almy.js
@@ -1,31 +1,34 @@
-;(function(exports) {
-    var state = {},
-        listeners = {}
-    exports.almy = {
-        newInstance: function newInstance() {
-            state = {}
-            listeners = {}
-        },
-        getState: function() {
-            return state
-        },
-        dispatch: function(key, value) {
-            state[key] = value
-            if(typeof listeners[key] === 'undefined') {
-                return
-            }
-            for(var i = 0; i < listeners[key].length; ++i) {
-                listeners[key][i](value)
-            }
-        },
-        subscribe: function(key, callback) {
-            if(typeof listeners[key] === 'undefined') {
-                listeners[key] = []
-            }
-            listeners[key].push(callback)
-            if(typeof state[key] !== 'undefined') {
-                callback(state[key])
-            }
-        }
+;(function (exports) {
+  var state = {}
+  var listeners = {}
+  exports.almy = {
+    newInstance: function () {
+      state = {}
+      listeners = {}
+    },
+    getState: function () {
+      return state
+    },
+    dispatch: function (key, value) {
+      if (!key || typeof key !== 'string') {
+        return
+      }
+      state[key] = value
+      if (typeof listeners[key] === 'undefined') {
+        return
+      }
+      for (var i = 0; i < listeners[key].length; ++i) {
+        listeners[key][i](value)
+      }
+    },
+    subscribe: function (key, callback) {
+      if (typeof listeners[key] === 'undefined') {
+        listeners[key] = []
+      }
+      listeners[key].push(callback)
+      if (typeof state[key] !== 'undefined') {
+        callback(state[key])
+      }
     }
-}(typeof module === 'object' ? module.exports : window));
+  }
+}(typeof module === 'object' ? module.exports : window))
